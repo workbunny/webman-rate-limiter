5430786edcbb5fb281606906c4f4d035973ec5f8
sunagne <583985775@qq.com>
2022-09-20 18:14:00 +0800

    config.

--- src/config/plugin/workbunny/webman-rate-limiter/app.php
+++ src/config/plugin/workbunny/webman-rate-limiter/app.php
@@ -2,12 +2,18 @@
 return [
     'enable' => true,
 
+    /** sqlite 数据库配置 */
     'sqlite' => [
+        /** 数据库文件地址 */
         "dbFilePath" => dirname(__DIR__) . "/ExampleDb/rate-limit.db",
+        /** 加密秘钥 */
         "encryptionKey" => ""
     ],
+    /** 令牌桶配置 */
     'bucket' => [
+        /** 桶的最大容量 */
         "capacity" => 60,
+        /** 满桶的所需时间 */
         "seconds" => 60
     ]
 
