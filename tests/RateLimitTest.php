df24bd861cec804123c3bb77de80f2c291b5026d
sunagne <583985775@qq.com>
2022-09-20 17:27:59 +0800

    config.初始化

--- tests/RateLimitTest.php
+++ tests/RateLimitTest.php
@@ -10,15 +10,7 @@ class RateLimitTest extends TestCase
 
     public function testHandle()
     {
-//        dump($a = hrtime(true));
-//        sleep(1);
-//        dump($b =  hrtime(true));
-//
-//
-//        dump($b-$a);
-//        dump(($b-$a) / 1000000000);
         $res = (new RateLimiter())->handle("127.0.0.1" , 60 , 60);
-        dump($res);
         $this->assertEquals(59, $res);
 
 
@@ -28,7 +20,6 @@ class RateLimitTest extends TestCase
     public function testHandle2()
     {
         $res = (new RateLimiter())->handle("127.0.0.2" , 1 , 60);
-        dump($res);
         $this->assertEquals(0, $res);
     }
 
